0e1919eebf569f1392485275827ce6c319857993
thiagobrito <thiago7brito@gmail.com>
2024-08-02 14:33:05 -0300

    v-for loop

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
   import Banner from '@components/Banner.vue';
+  import MainContent from './components/MainContent.vue';
+
   export default {
     components: {
       Banner,
+      MainContent,
     },
   };
 </script>
 
 <template>
   <Banner />
+  <MainContent />
 </template>
 
 <style scoped>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,97 @@
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                ingredients: [
+                    'Alho',
+                    'Manteiga',
+                    'Orégano',
+                    'Pão',
+                    'Tomilho',
+                    'Limão',
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <section>
+            <span class="subtitulo-lg sua-lista-texto">
+                Sua lista:
+            </span>
+
+            <ul class="ingredientes-sua-lista">
+                <li v-for="ingredient in ingredients" class="ingrediente">
+                    {{ ingredient }}
+                </li>
+            </ul>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+    .conteudo-principal {
+        padding: 6.5rem 7.5rem;
+        border-radius: 3.75rem 3.75rem 0rem 0rem;
+        background: var(--creme, #FFFAF3);
+        color: var(--cinza, #444);
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 5rem;
+    }
+
+    .sua-lista-texto {
+        color: var(--coral, #F0633C);
+        display: block;
+        text-align: center;
+        margin-bottom: 1.5rem;
+    }
+
+    .ingredientes-sua-lista {
+        display: flex;
+        justify-content: center;
+        gap: 1rem 1.5rem;
+        flex-wrap: wrap;
+    }
+
+    .ingrediente {
+        display: inline-block;
+        border-radius: 0.5rem;
+        min-width: 4.25rem;
+        padding: 0.5rem;
+        text-align: center;
+        transition: 0.2s;
+        color: var(--creme, #FFFAF3);
+        background: var(--coral, #F0633C);
+        font-weight: 700;
+    }
+
+    .lista-vazia {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 0.25rem;
+
+        color: var(--coral, #F0633C);
+        text-align: center;
+    }
+
+    @media only screen and (max-width: 1300px) {
+        .conteudo-principal {
+            padding: 5rem 3.75rem;
+            gap: 3.5rem;
+        }
+    }
+
+    @media only screen and (max-width: 767px) {
+        .conteudo-principal {
+            padding: 4rem 1.5rem;
+            gap: 4rem;
+        }
+    }
+</style>
