d06d4802db7cfbf7355cf1ee1310e87b22c2353a
thiagobrito <thiago7brito@gmail.com>
2024-08-05 21:56:51 -0300

    dynamic importing

--- src/components/CardCategory.vue
+++ src/components/CardCategory.vue
@@ -13,8 +13,57 @@
 </script>
 
 <template>
-    {{ category.nome }}
+    <article class="categoria">
+        <header class="categoria__cabecalho">
+            <img :src="`/images/icons/categorias_ingredientes/${category.imagem}`" alt="" class="categoria__imagem">
+
+            <h2 class="paragrafo-lg categoria__nome">{{ category.nome }}</h2>
+        </header>
+
+        <ul class="categoria__ingredientes">
+            <li v-for="ingredient in category.ingredientes" :key="ingredient">
+                {{ ingredient }}
+            </li>
+        </ul>
+    </article>
 </template>
 
 <style scoped>
+    .categoria {
+        width: 19.5rem;
+        padding: 1rem;
+        border-radius: 1rem;
+        background: var(--branco, #FFF);
+        box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+        height: 100%;
+    
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+    }
+  
+    .categoria__cabecalho {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    .categoria__imagem {
+        width: 3.5rem;
+    }
+
+    .categoria__nome {
+        text-align: center;
+        color: var(--verde-medio, #3D6D4A);
+        font-weight: 700;
+    }
+
+    .categoria__ingredientes {
+        display: flex;
+        justify-content: center;
+        gap: 0.5rem;
+        flex-wrap: wrap;
+    }
 </style>
